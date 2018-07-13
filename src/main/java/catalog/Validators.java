2ded0b1b786edbd2a187f75001b499aa45df8dba
Andrea Marton <andi.marton@gmail.com>
2018-07-13 11:21:18 +0200

    Validators and Features

--- src/main/java/catalog/Validators.java
+++ src/main/java/catalog/Validators.java
@@ -1,5 +1,19 @@
 package catalog;
 
+import java.util.List;
+
 public class Validators {
 
+    public static boolean isEmpty(List<Object> list) {
+        return list == null || list.size() == 0;
+    }
+
+    public static boolean isBlank(String string) {
+        return string == null || string.trim().length() == 0;
+    }
 }
+
+
+
+
+
